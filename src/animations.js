79e39eb5410911e8ff6d7f8d929b650cf634a49d
Diego Mendes <mendesdiego818@gmail.com>
2025-08-24 11:39:49 -0300

    info section
    Terminei a sessão de informações sobre o gdae, tanto mobile quanto desk

---
+++ src/animations.js
@@ -0,0 +1,76 @@
+//evento de troca de imagem de fundo:
+document.addEventListener("DOMContentLoaded", ()  => {
+
+    const images = [
+    '../assets/images/Foto-grupo-1.jpeg',
+    '../assets/images/foto-grupo-2.jpeg',
+    '../assets/images/foto-grupo-3.png',
+    '../assets/images/foto-grupo-4.png'
+    ];
+
+    let index = 0;
+    const bg = document.querySelector('.bg-slider');
+    // const bg = document.body;
+
+    function changeBackground() {
+    index = (index + 1) % images.length;
+    console.log('Trocando para:', images[index]);
+    bg.style.backgroundImage = `url(${images[index]})`;
+    }
+
+    bg.style.backgroundImage = `url(${images[index]})`;
+
+    setInterval(changeBackground, 5000); // troca a cada 5s
+
+})
+
+
+//evento do menu hamburguer:
+document.addEventListener("DOMContentLoaded", () => {
+  const checkbox = document.getElementById("checkbox");
+  const menu = document.querySelector(".header__menu");
+  const logo = document.querySelector(".header__logo");
+
+  function sync() {
+    if (checkbox.checked) {
+      menu.classList.add("ativo");
+      logo.classList.add("hidden");
+    } else {
+      menu.classList.remove("ativo");
+      logo.classList.remove("hidden");
+    }
+  }
+
+  checkbox.addEventListener("change", sync);
+
+  // Reset quando for > 768px (evita ficar escondido no desktop)
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+      checkbox.checked = false;
+      menu.classList.remove("ativo");
+      logo.classList.remove("hidden");
+    }
+  });
+
+
+  sync();
+});
+
+//evento do slider:
+document.addEventListener("DOMContentLoaded", () => {
+  let count2 = 1;
+  const links = document.querySelectorAll(".slider-nav a");
+
+  setInterval(() => {
+    if( count2 < links.length){
+      links[count2].click();
+      count2++;
+    }
+    else{
+      count2=0;
+      links[count2].click();
+      count2++;
+    }
+  }, 5000)
+
+});
